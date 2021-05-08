b0faf3f5fc0efc2abf4ba0aaf19845b93edbb29c
Maxson Coelho <maxsoncoelho@gmail.com>
2021-05-08 14:45:22 -0400

    creation and structure of web design and header

---
+++ Project-ToDo-organization-life/front-web/src/views/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+import * as S from './styles';
+
+function Home() {
+  return (
+    <S.Container>
+        <Header />
+    </S.Container>
+  );
+}
+
+export default Home;
